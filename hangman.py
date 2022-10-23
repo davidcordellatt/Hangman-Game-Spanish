680ee32db171d0c4fd928c114e6ee1244d6a821a
David Cordellat <davidcordellatt.lite@gmail.com>
2022-10-23 14:26:48 -0400

    Formatos agreados a los mensajes

--- hangman.py
+++ hangman.py
@@ -3,9 +3,9 @@ import random
 
 name = input("¿Cual es tu nombre?: ")
 
+os.system('clear')
 
-
-creditos = """
+credits = """
 El Ahorcado - Autor: David Cordellat - Made in Venezuela 
 """
 def random_word(filepath="./archivos/data.txt"):
@@ -27,24 +27,24 @@ def select_word():
         ocult_word_index[letter].append(a)
     
     while True:
-            
-            print("Buena suerte, " + name)
+            os.system("clear")
+            print(f"Buena suerte, {name}" )
             print("¡Adivina la palabra!")
             for under in ocult_word:
                 print(under + " ", end=" ")
             print("")
 
             letter = input("Ingresa una letra: ").strip().upper()
-            assert letter.isalpha(), "Solo puedes ingresar letras"
+            assert letter.isalnum (), "Solo puedes ingresar letras"
 
             if letter in select_word_list:
                 for idx in ocult_word_index[letter]:
                     ocult_word[idx] = letter
 
             if "_" not in ocult_word:
-                
-                print("¡Felicidades! " + name + " La palabra era", select_word)
-                print(creditos)
+                os.system("clear")
+                print(f"¡Felicidades! {name}, la palabra era {select_word}")
+                print(credits)
                 break
 
 select_word()
