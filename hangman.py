7e6902b7a3d7b9b38881d22ca02904207c21b994
David Cordellat <davidcordellatt.lite@gmail.com>
2022-10-23 14:45:42 -0400

    Sistema de intentos agregados + los clear

--- hangman.py
+++ hangman.py
@@ -1,14 +1,13 @@
 import os
 import random
 
+os.system("clear")
 name = input("¿Cual es tu nombre?: ")
 
-os.system('clear')
-
-credits = """
+creditos = """
 El Ahorcado - Autor: David Cordellat - Made in Venezuela 
 """
-def random_word(filepath="./archivos/data.txt"):
+def random_word(filepath="./data.txt"):
     words = []
     with open(filepath, "r", encoding="utf-8") as f:
         for line in f:
@@ -16,7 +15,8 @@ def random_word(filepath="./archivos/data.txt"):
         return(words)
            
 def select_word():
-    data = random_word(filepath="./archivos/data.txt")
+    tries = 5
+    data = random_word(filepath="./data.txt")
     select_word = random.choice(data)
     select_word_list = [letter for letter in select_word]
     ocult_word = ["_"] * len(select_word_list)
@@ -27,24 +27,35 @@ def select_word():
         ocult_word_index[letter].append(a)
     
     while True:
+
             os.system("clear")
-            print(f"Buena suerte, {name}" )
+            print(f"Buena suerte, {name}")
             print("¡Adivina la palabra!")
+            print(f"Tienes {tries} intentos")
             for under in ocult_word:
                 print(under + " ", end=" ")
             print("")
 
             letter = input("Ingresa una letra: ").strip().upper()
-            assert letter.isalnum (), "Solo puedes ingresar letras"
+            assert letter.isalpha (), "Solo puedes ingresar letras"
 
             if letter in select_word_list:
                 for idx in ocult_word_index[letter]:
                     ocult_word[idx] = letter
 
+            else:
+                tries -= 1
+
+                if tries == 0:
+                  os.system("clear")
+                  
+                  print(f"{name} ¡Perdiste!, la palabra correcta era {select_word}" )
+                  break
+
             if "_" not in ocult_word:
-                os.system("clear")
+                
                 print(f"¡Felicidades! {name}, la palabra era {select_word}")
-                print(credits)
+                print(creditos)
                 break
 
 select_word()
