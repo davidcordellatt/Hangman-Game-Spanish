c5b4cb0d5cdc51a1f6e33ec400492bb76838582a
David Cordellat <davidcordellatt.lite@gmail.com>
2022-10-25 23:38:57 -0400

    actualización de hangman

--- hangman.py
+++ hangman.py
@@ -1,8 +1,10 @@
 import os
 import random
 
-os.system("clear")
 name = input("¿Cual es tu nombre?: ")
+name = name.strip().capitalize()
+
+os.system('clear')
 
 creditos = """
 El Ahorcado - Autor: David Cordellat - Made in Venezuela 
@@ -15,7 +17,7 @@ def random_word(filepath="./data.txt"):
         return(words)
            
 def select_word():
-    tries = 5
+    y = 0
     data = random_word(filepath="./data.txt")
     select_word = random.choice(data)
     select_word_list = [letter for letter in select_word]
@@ -25,39 +27,57 @@ def select_word():
         if not ocult_word_index.get(letter): 
             ocult_word_index[letter] = []
         ocult_word_index[letter].append(a)
-    
+      
+    for i in select_word:
+      y += 0.75
+      tries = int(y)
+      
     while True:
-
+            
             os.system("clear")
-            print(f"Buena suerte, {name}")
+            print(f"Buena suerte, {name}.")
             print("¡Adivina la palabra!")
             print(f"Tienes {tries} intentos")
+                      
+      
             for under in ocult_word:
                 print(under + " ", end=" ")
             print("")
 
-            letter = input("Ingresa una letra: ").strip().upper()
-            assert letter.isalpha (), "Solo puedes ingresar letras"
+            letter = input("\n" "Ingresa una letra: ").strip().upper()
 
+            if not letter.isalpha():
+              os.system("clear")
+              raise Exception('No se permite usar numeros')
+              continue
+              select_word()
+              
+            if letter == select_word:
+              os.system("clear")
+              print(f"¡Muy bien {name}!, la palabra correcta era {select_word}" )
+              break
+              
+      
             if letter in select_word_list:
                 for idx in ocult_word_index[letter]:
                     ocult_word[idx] = letter
-
-            else:
-                tries -= 1
+                  
+            if letter not in select_word_list:
+                tries = tries - 1
 
                 if tries == 0:
                   os.system("clear")
-                  
                   print(f"{name} ¡Perdiste!, la palabra correcta era {select_word}" )
                   break
-
+      
             if "_" not in ocult_word:
-                
-                print(f"¡Felicidades! {name}, la palabra era {select_word}")
+                os.system("clear")
+                print("¡Felicidades! " + name + " La palabra era", select_word)
                 print(creditos)
                 break
-
+              
+            
+        
 select_word()
 
 if __name__ == '__main__':
